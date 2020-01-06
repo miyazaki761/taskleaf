b9572a795f92efe41023ef4346633c9aa85243c3
a <miyazaki.a0806@gmail.com>
2020-01-06 23:48:56 +0900

    Add: order, scope, set_task, autolink

--- db/migrate/20200102131316_add_admin_to_users.rb
+++ db/migrate/20200102131316_add_admin_to_users.rb
@@ -1,5 +1,6 @@
 class AddAdminToUsers < ActiveRecord::Migration[5.2]
   def change
-  	add_column :users, :admin, :boolean, default: false, null: false
+  	add_column :users, :admin, :boolean, default: false
+  	change_column :users, :admin, :boolean, :null => false
   end
 end
